d214fcbef9f9660a25067a6defda24474055fb52
zhihaowang <zhihaowang@tencent.com>
2020-02-04 22:14:37 +0800

    提交代码

---
+++ routers/router.go
@@ -0,0 +1,19 @@
+package routers
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jihite/go-gin-example/pkg/setting"
+)
+
+func InitRouter() *gin.Engine {
+	r := gin.New()
+	r.Use(gin.Logger())
+	r.Use(gin.Recovery())
+	gin.SetMode(setting.RunMode)
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	return r
+}
